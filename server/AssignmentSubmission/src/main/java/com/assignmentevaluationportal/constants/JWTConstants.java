c579746edf2f8556a222b863ddeb3d9c85c20b0f
Tushar Sugandhi <sugandhi.tushar@gmail.com>
2019-10-02 23:25:26 +0530

    Added swagger config for bearer authentication. Increased access token and refresh token expiry time.

--- server/AssignmentSubmission/src/main/java/com/assignmentevaluationportal/constants/JWTConstants.java
+++ server/AssignmentSubmission/src/main/java/com/assignmentevaluationportal/constants/JWTConstants.java
@@ -2,7 +2,7 @@ package com.assignmentevaluationportal.constants;
 
 public class JWTConstants {
 
-	public static final Long JWT_ACCESS_TOKEN_VALIDITY_IN_MINUTES = 1L;
-	public static final Long JWT_REFRESH_TOKEN_VALIDITY_IN_MINUTES = 30L;
+	public static final Long JWT_ACCESS_TOKEN_VALIDITY_IN_MINUTES = 30L;
+	public static final Long JWT_REFRESH_TOKEN_VALIDITY_IN_MINUTES = 60L;
 
 }
